91a587884a0281ba3829111fed216b6d0a9b8720
maien <maien@work.local>
2026-06-03 15:32:20 +0800

    Fix JUnit Platform launcher dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,8 @@ repositories {
 dependencies {
     implementation("org.antlr:antlr4:4.13.2")
     implementation("org.slf4j:slf4j-simple:2.0.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
